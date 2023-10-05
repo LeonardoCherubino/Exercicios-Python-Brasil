4852b6fd8ab05737ca3a3ae69705df2d376a5446
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-04 22:18:54 -0300

    Python Brasil - Exercício 03
    Python Brasil - Exercício 03

--- Exercício 03.py
+++ Exercício 03.py
@@ -4,3 +4,4 @@ Num1 = int(input('Entre com um número. '))
 Num2 = int(input('Entre com o segundo número. '))
 Soma = Num1 + Num2
 print('{} + {} = {}.'.format(Num1, Num2, Soma))
+print('Obrigado por usar nossos programas. ')
