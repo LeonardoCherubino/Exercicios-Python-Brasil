9041791998ddc06221806639018b377832b2e239
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-04 21:59:30 -0300

    Python Brasil - Exercício 04
    Python Brasil - Exercício 04

---
+++ Exercício 04.py
@@ -0,0 +1,8 @@
+## Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+Nota1 = int(input('Entre com a primeira nota. '))
+Nota2 = int(input('Entre com a segunda nota.'))
+Nota3 = int(input('Entre com a terceira nota.'))
+Nota4 = int(input('Entre com a quarta nota.'))
+Média = (Nota1 + Nota2 + Nota3 + Nota4) / 4
+print('A média do aluno foi {}.'.format(Média))
