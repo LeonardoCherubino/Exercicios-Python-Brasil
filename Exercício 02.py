0d908fe4d12e5864ea91b8b7b26f59dd42f0f6f2
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-04 22:17:32 -0300

    Python Brasil - Exercício 02
    Python Brasil - Exercício 02

--- Exercício 02.py
+++ Exercício 02.py
@@ -2,4 +2,4 @@
 
 Num1 = int(input('Entre com um número. '))
 print('O número informado foi {}.'.format(Num1))
-print('Obrigado por usar nossos sistemas. ')
+print('Obrigado por usar nossos sistemas.  ')
