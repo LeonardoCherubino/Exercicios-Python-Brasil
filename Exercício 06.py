6f64f77ee6a8e6c0a1aeacafa9736087105b12cd
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-04 22:15:45 -0300

    Python Brasil - Exercício 06
    Python Brasil - Exercício 06

---
+++ Exercício 06.py
@@ -0,0 +1,6 @@
+## Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+## A = π r²
+
+Raio = float(input('Entre com o raio do círculo em metros. '))
+A = 3.14 * Raio ** 2
+print('A área desse círculo é {} metros².'.format(A))
