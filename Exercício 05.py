b921ffc625c9ab0815c10a411312c3843bc0101c
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-04 22:07:46 -0300

    Python Brasil - Exercício 05
    Python Brasil - Exercício 05

---
+++ Exercício 05.py
@@ -0,0 +1,5 @@
+## Faça um Programa que converta metros para centímetros.
+
+Metros = float(input('Entre com o valor em metros. '))
+Centímetros = Metros * 100
+print('O valor correspondente de {} metro(s) são {} centímetro(s). '.format(Metros, Centímetros))
