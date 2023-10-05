ba4146930b57f2328efb9cf65730f34d0b0fe4c2
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-04 21:49:33 -0300

    Python Brasil - Exercício 3.
    Faça um Programa que peça dois números e imprima a soma.

---
+++ Exercício 03.py
@@ -0,0 +1,6 @@
+## Faça um Programa que peça dois números e imprima a soma.
+
+Num1 = int(input('Entre com um número. '))
+Num2 = int(input('Entre com o segundo número. '))
+Soma = Num1 + Num2
+print('{} + {} = {}.'.format(Num1, Num2, Soma))
