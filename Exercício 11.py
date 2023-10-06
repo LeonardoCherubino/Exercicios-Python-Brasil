352a4366bc27c4601e4208cf5478b8bcaa4c2d37
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2023-10-05 22:35:01 -0300

    Python Brasil - Exercício 11
    Python Brasil - Exercício 11

---
+++ Exercício 11.py
@@ -0,0 +1,14 @@
+## Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+## o produto do dobro do primeiro com metade do segundo .
+## a soma do triplo do primeiro com o terceiro.
+## o terceiro elevado ao cubo.
+
+Num1 = int(input('Entre com um número inteiro. '))
+Num2 = int(input('Entre com mais um número inteiro. '))
+Num3 = float(input('Entre com um número real. '))
+a = (Num1 * 2) * (Num2 / 2)
+print(a)
+b = (Num1 * 3) + (Num3)
+print(b)
+c = (Num3 ** 3)
+print(c)
